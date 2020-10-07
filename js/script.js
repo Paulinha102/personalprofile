88f5cb195069270c2d70d3953713a0e7ae338f1f
Paulinha102 <58919770+Paulinha102@users.noreply.github.com>
2020-10-06 22:31:13 -0300

    Reiniciando Layout

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function initMenu(){
     window.addEventListener('scroll', fixedMenu);
 
 }
-//initMenu();
+initMenu();
 
 
 function initScrollSuave(){
@@ -46,27 +46,3 @@ function initScrollSuave(){
     });
 }
 //initScrollSuave();
-
-
-function initScrollPortfolio(){
-    const spaceHolder = document.querySelector('.space-holder');
-    const horizontal = document.querySelector('.horizontal');
-    spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
-
-    function calcDynamicHeight(ref) {
-    const vw = window.innerWidth;
-    const vh = window.innerHeight;
-    const objectWidth = ref.scrollWidth;
-    return objectWidth - vw + vh + 150;
-    }
-
-    window.addEventListener('scroll', () => {
-    const sticky = document.querySelector('.sticky');
-    horizontal.style.transform = `translateX(-${sticky.offsetTop}px)`;
-    });
-
-    window.addEventListener('resize', () => {
-    spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
-    });
-}
-initScrollPortfolio();
